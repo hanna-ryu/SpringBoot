38a04014a6c3b46fc1e59505d420f28d08260b2e
Hanna <foureaf@gmail.com>
2023-10-22 14:52:36 +0900

    [섹션 5-2 회원 웹 기능-등록]

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller //스프링 컨테이너에 MemberController의 객체를 생성해서 관리하고있음 / 기본적으로 싱글톤으로 생성됨.
 public class MemberController {
@@ -17,8 +20,21 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-
+    @GetMapping("/members/new")
+    public  String createForm(){
+        return "members/createMemberForm";
+    }
     //실무에서는 주로 정형화된 컨트롤러, 서비스, 리포지토리 같은 코드는 컴포넌트 스캔을 이용함.
     //그리고 정형화되지 않거나 상황에 따라 구현 클래스를 변경해야하면 설정을 통해 스프링 빈으로 이동한다.
 
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+
+    }
 }
