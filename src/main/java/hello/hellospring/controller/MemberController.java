5146bc12cb8dec8087b7c7c303a88036cb56f6a7
Hanna <foureaf@gmail.com>
2023-10-22 15:11:44 +0900

    [섹션 5-3 회원 웹 기능-조회]

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller //스프링 컨테이너에 MemberController의 객체를 생성해서 관리하고있음 / 기본적으로 싱글톤으로 생성됨.
 public class MemberController {
@@ -35,6 +38,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
 
+    @GetMapping("/members")
+    public String List(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return  "members/memberList";
     }
 }
